19033ad0ee6174255e16ff3d1adbba7092f563db
yunushan <yunushangoksu@gmail.com>
2023-07-31 20:07:45 +0300

    finally managed to go through painful phase of configuring webpack

--- src/pages/landingPage.js
+++ src/pages/landingPage.js
@@ -1,6 +1,5 @@
 const landingPage = () => {
   console.log("works");
-
   const body = document.querySelector("#content");
   let yazi = document.createElement("p");
   yazi.textContent = "Yunushan";
